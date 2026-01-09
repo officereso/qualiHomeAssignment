e8d3d22769d5aacb619acac82566ab1e0700a018
officereso <qrs01@snowtx.com>
2026-01-09 14:27:59 -0600

    Added descriptions to variables and renamed assets

--- assets/terriform/aws/ec2_ssh_http_open/variables.tf
+++ assets/terriform/aws/ec2_ssh_http_open/variables.tf
@@ -1,43 +1,51 @@
 variable "region" {
+	description = "AWS Region to create resource."
 	default = "us-east-2"
 	type = string
 	nullable = false
 }
 
 variable "ami" {
+	description = "AMI to launch with. Choose one from the Amazon AMI catalog."
 	type = string
 	nullable = false
 }
 
 variable "instance_type" {
+	description = "A compatible instance type for this AMI."
 	default = "t2.micro"
 	type = string
 	nullable = false
 }
 
 variable "name" {
+	description = "Name of this resource in AWS."
 	default = null
 	type = string
 	nullable = true
 }
 
 variable "subnet_id" {
+	description = "Subnet to associate this EC2 instance in."
 	type = string
 	nullable = false
 }
 
 variable "private_ips" {
+	description = "Set of private IP's to give this EC2 resource. IP's listed must reside within the CIDR of the associated subnet_id."
 	type = set(string)
 	nullable = false
 }
 
 variable "vpc_id" {
+	description = "ID of the VPC to associate this EC2 resource with."
 	default = false
 	type = string
 	nullable = false
 }
 
 variable "key_name" {
+	description = "Name identifying Key Pair to SSH into instance with."
 	type = string
 	nullable = false
 }
--- assets/terriform/aws/vpc_public/variables.tf
+++ assets/terriform/aws/vpc_public/variables.tf
@@ -1,16 +1,19 @@
 variable "region" {
+	description = "AWS Region to create resource."
 	default = "us-east-2"
 	type = string
 	nullable = false
 }
 
 variable "cidr_block" {
+	description = "CIDR block for this VPC."
 	default = "10.0.0.0/16"
 	type = string
 	nullable = false
 }
 
 variable "name" {
+	description = "Name of this resource in AWS."
 	default = null
 	type = string
 	nullable = true
